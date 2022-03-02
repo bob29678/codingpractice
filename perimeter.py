e354b1ff5a2ecbf50c35b3e51a32c76d50c5b12f
bob29678 <50780120+bob29678@users.noreply.github.com>
2022-03-02 10:44:10 +0000

    Update and rename perimeter to perimeter.py

--- perimeter.py
+++ perimeter.py
@@ -1,10 +1,13 @@
+#This program finds the perimeter of a given triangle
+#This may be changed in the future to find other properties such as area, or perimeter of other polygons
+
 import math
 
 def perimeter(a,b,c):
     return(pointdistance(a,b)+pointdistance(a,c)+pointdistance(b,c))
 
 
-def pointdistance(point1,point2):
+def pointdistance(point1,point2): #Finds distance between two input points
     x=point1[0]-point2[0]
     y=point1[1]-point2[1]
     return math.sqrt(x**2+y**2)
