58241e119e6bf639604e6290782cc0cda19d80f2
bob29678 <50780120+bob29678@users.noreply.github.com>
2022-03-03 12:00:16 +0000

    Update and rename unfinished code.py to Economical.py

--- Economical.py
+++ Economical.py
@@ -12,7 +12,7 @@ def findprimefactors(x):
                 break
             else:
                 y+=1
-    return factorlist
+    return simplifylist(factorlist)
 
 def simplifylist(x): #Simplifies a list of numbers, eg. 4*4*4*4 -> 4^4
     simplifiedlist=[]
@@ -23,7 +23,17 @@ def simplifylist(x): #Simplifies a list of numbers, eg. 4*4*4*4 -> 4^4
 
 
 def isEconomical(x):
-    return(x)
+    primefactors=findprimefactors(x)
+    total=0
+    for i in primefactors:
+        total+=len(str(i[0]))
+        if i[1]!= 1:
+            total+=len(str(i[1]))
+    if total == len(str(x)):
+        return("Perfectly economical")
+    elif total < len(str(x)):
+        return("More than economical")
+    else:
+        return("Expensive!")
 
-print(simplifylist(findprimefactors(54)))
-
+print(isEconomical(54))
